a06f9a62edb15f1b6d541c9867484e01cc9d4b1c
lukepatrick <lukephilips@gmail.com>
2017-11-15 13:51:46 -0700

    more js experiments

--- brigade.js
+++ brigade.js
@@ -7,8 +7,12 @@ events.on("exec", (brigadeEvent, project) => {
     var goodbye = new Job("goodbye", "alpine:3.4", ["echo goodbye"])
     var helloAgain = new Job("hello-again", "alpine:3.4", ["echo hello again"])
     
-    Group.runAll([hello, goodbye]).then( ()=> {
-    helloAgain.run()
+    Group.runAll([hello, goodbye]).then( (result)=> {
+    	console.log(result)
+    	console.log("\n result = " + result.toString() + "\n")
+    	helloAgain.run().then( (resultB) => {
+    		console.log("\n resultB = " + resultB.toString() + "\n")
+    	})
 	//console.log("Hello world!")
 })
 })
