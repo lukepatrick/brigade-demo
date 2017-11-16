6dc090ccb04faae28c002022ae5b106f8da340f4
lukepatrick <lukephilips@gmail.com>
2017-11-16 13:12:13 -0700

    no message

--- brigade.js
+++ brigade.js
@@ -3,13 +3,13 @@ const { events, Job, Group } = require("brigadier")
 events.on("exec", (brigadeEvent, project) => {
 
 
-    var hello = new Job("hello", "alpine:3.4", ["echo hello"])
+    var hello = new Job("hello", "alpine:3.4", ["echo hello", "uname -a", "ls -lgra", "pwd"])
     var goodbye = new Job("goodbye", "alpine:3.4", ["echo goodbye"])
-    var helloAgain = new Job("hello-again", "alpine:3.4", ["echo hello again"])
+    var helloAgain = new Job("hello-again", "quay.io/charter-os/kolla-build:v0.1.3", ["which kolla-build", "hostname", "uname -a"])
     
     Group.runAll([hello, goodbye]).then( (result)=> {
-    	console.log(result)
-    	console.log("\n result = " + result.toString() + "\n")
+    	console.log("\n result = " + result[0].toString() + "\n")
+    	console.log("\n result = " + result[1].toString() + "\n")
     	helloAgain.run().then( (resultB) => {
     		console.log("\n resultB = " + resultB.toString() + "\n")
     	})
